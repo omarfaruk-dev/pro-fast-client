b5c844bc5e2127bbdd2a60a518f699811c26275b
Omar Faruk <omarfaruk.dev@gmail.com>
2025-06-25 16:05:19 +0600

    send a parcel page form data created

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -24,6 +24,7 @@ export const router = createBrowserRouter([
       {
         path: 'send-parcel',
         element: <PrivateRoute><SendParcel /></PrivateRoute>,
+        loader: () => fetch("./serviceCenter.json")
       }
     ]
   },
